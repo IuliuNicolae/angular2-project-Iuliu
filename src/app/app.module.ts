de95e4bebb1962d68c9d9262bfc9fb4831c95cbe
IuliuNicolae <iuliunicolaebarcan@gmail.com>
2017-06-22 12:59:38 +0200

    Angular2 Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+import { routes } from './app.router';
 
+import 'hammerjs';
+
+import { ComicService } from './services/comic.service';
+import { StateService } from './services/state.service';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { NewComicComponent } from './new-comic/new-comic.component';
+import { LastComicComponent } from './last-comic/last-comic.component';
+import { ComicComponent } from './comic/comic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidenavComponent,
+    NewComicComponent,
+    LastComicComponent,
+    ComicComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    JsonpModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    routes
   ],
-  providers: [],
+  exports: [
+    MaterialModule
+  ],
+  providers: [
+    ComicService,
+    StateService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
